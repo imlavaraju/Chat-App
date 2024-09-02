120060e1abcc827864c8057e391096027359f88b
LAVARAJU GORLI <141550385+imlavaraju@users.noreply.github.com>
2024-09-02 16:02:54 +0530

    Update Sidebar.js

--- client/src/Components/Sidebar.js
+++ client/src/Components/Sidebar.js
@@ -41,7 +41,7 @@ function Sidebar() {
       },
     };
 
-    axios.get("http://localhost:8080/chat/", config).then((response) => {
+    axios.get("https://chat-app-backend-q49dad3j0-lavarajus-projects.vercel.app/chat/", config).then((response) => {
       console.log("Data refresh in sidebar ", response.data);
       setConversations(response.data);
       // setRefresh(!refresh);
